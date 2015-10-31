913884252f4d4e9ac1b2ce8b28dbf33c3f715b67
ShekharKG <shekhar114@gmail.com>
2015-10-31 14:29:06 +0530

    First frament selected on create of activity

--- app/src/main/java/com/shekharkg/tradelab/MainActivity.java
+++ app/src/main/java/com/shekharkg/tradelab/MainActivity.java
@@ -2,7 +2,6 @@ package com.shekharkg.tradelab;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -48,6 +47,7 @@ public class MainActivity extends AppCompatActivity
 
     NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     navigationView.setNavigationItemSelectedListener(this);
+    getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
   }
 
   @Override
@@ -59,28 +59,6 @@ public class MainActivity extends AppCompatActivity
       super.onBackPressed();
     }
   }
-
-//  @Override
-//  public boolean onCreateOptionsMenu(Menu menu) {
-//    // Inflate the menu; this adds items to the action bar if it is present.
-//    getMenuInflater().inflate(R.menu.main, menu);
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean onOptionsItemSelected(MenuItem item) {
-//    // Handle action bar item clicks here. The action bar will
-//    // automatically handle clicks on the Home/Up button, so long
-//    // as you specify a parent activity in AndroidManifest.xml.
-//    int id = item.getItemId();
-//
-//    //noinspection SimplifiableIfStatement
-//    if (id == R.id.action_settings) {
-//      return true;
-//    }
-//
-//    return super.onOptionsItemSelected(item);
-//  }
 
   @Override
   public boolean onNavigationItemSelected(MenuItem item) {
--- app/src/main/java/com/shekharkg/tradelab/fragment/AddNewEntryFragment.java
+++ app/src/main/java/com/shekharkg/tradelab/fragment/AddNewEntryFragment.java
@@ -1,12 +1,13 @@
 package com.shekharkg.tradelab.fragment;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.iangclifton.android.floatlabel.FloatLabel;
 import com.shekharkg.tradelab.R;
 
 /**
@@ -14,13 +15,24 @@ import com.shekharkg.tradelab.R;
  */
 public class AddNewEntryFragment extends Fragment {
 
+  private FloatLabel exchange, product, underline, expiry, type, strike, lta;
+  private Button addButton;
+
   public AddNewEntryFragment() {
   }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-    return inflater.inflate(R.layout.fragment_add_new_entry, container, false);
+    View v = inflater.inflate(R.layout.fragment_add_new_entry, container, false);
+    exchange = (FloatLabel) v.findViewById(R.id.exchange);
+    product = (FloatLabel) v.findViewById(R.id.product);
+    underline = (FloatLabel) v.findViewById(R.id.underline);
+    expiry = (FloatLabel) v.findViewById(R.id.expiry);
+    type = (FloatLabel) v.findViewById(R.id.type);
+    strike = (FloatLabel) v.findViewById(R.id.strike);
+    lta = (FloatLabel) v.findViewById(R.id.lta);
+    addButton = (Button) v.findViewById(R.id.addButton);
+    return v;
   }
 
   @Override
