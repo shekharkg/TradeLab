d6a207b58bac9f9546f50888c98eec1d8e532de5
ShekharKG <shekhar114@gmail.com>
2015-10-31 17:30:20 +0530

    Completed

--- app/src/main/java/com/shekharkg/tradelab/fragment/HomeFragment.java
+++ app/src/main/java/com/shekharkg/tradelab/fragment/HomeFragment.java
@@ -61,7 +61,10 @@ public class HomeFragment extends Fragment {
       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-          showDialog(position);
+          try {
+            showDialog(position);
+          } catch (Exception ignored) {
+          }
         }
       });
     } else {
